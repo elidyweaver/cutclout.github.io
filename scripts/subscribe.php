aa921d0a5d6455469f73b46792ef249d313f33bc
CutClout <119618812+elidyweaver@users.noreply.github.com>
2023-04-26 15:08:18 -0600

    cut clout: testing sign up for sunday zine

---
+++ scripts/subscribe.php
@@ -0,0 +1,32 @@
+<?php
+
+## CONFIG ##
+
+# LIST EMAIL ADDRESS
+$recipient = "air.element.1@proton.me";
+
+# SUBJECT (Subscribe/Remove)
+$subject = "Cut Clout Sign Up";
+
+# RESULT PAGE
+$location = "https://cutclout.com/thank-you/index.html";
+
+## FORM VALUES ##
+
+# SENDER - WE ALSO USE THE RECIPIENT AS SENDER IN THIS SAMPLE
+# DON'T INCLUDE UNFILTERED USER INPUT IN THE MAIL HEADER!
+$sender = $recipient;
+
+# MAIL BODY
+$body .= "Name: ".$_REQUEST['Name']." \n";
+$body .= "Npub: ".$_REQUEST['NostrNpub']." \n";
+# add more fields here if required
+
+## SEND MESSGAE ##
+
+mail( $recipient, $subject, $body, "From: $sender" ) or die ("Oh Oh! we couldn't sign you up.");
+
+## SHOW RESULT PAGE ##
+
+header( "Location: $location" );
+?>
